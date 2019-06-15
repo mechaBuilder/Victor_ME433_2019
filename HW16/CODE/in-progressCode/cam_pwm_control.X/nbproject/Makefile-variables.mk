135d69de36c77585587312915bc553e21265bfb7
mechaBuilder <49080979+mechaBuilder@users.noreply.github.com>
2019-06-15 03:30:09 -0500

    hw16

---
+++ HW16/CODE/in-progressCode/cam_pwm_control.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=cam_pwm_control.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/cam_pwm_control.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=campwmcontrol.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/campwmcontrol.x.tar
