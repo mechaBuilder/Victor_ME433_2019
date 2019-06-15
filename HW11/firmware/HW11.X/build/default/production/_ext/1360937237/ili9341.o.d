8ae12663880e10529754777dc886f515c6c811c0
mechaBuilder <49080979+mechaBuilder@users.noreply.github.com>
2019-06-15 04:07:29 -0500

    hw11 updated video

--- HW11/firmware/HW11.X/build/default/production/_ext/1360937237/ili9341.o.d
+++
@@ -1,3 +0,0 @@
-build/default/production/_ext/1360937237/ili9341.o:  \
-../src/ili9341.c  \
- ../src/ili9341.h 
--- HW11/firmware/HW11.X/build/default/production/_ext/1360937237/imu.o.d
+++
@@ -1,4 +0,0 @@
-build/default/production/_ext/1360937237/imu.o:  \
-../src/imu.c  \
-../src/imu.h  \
- ../src/i2c_master_noint.h 
